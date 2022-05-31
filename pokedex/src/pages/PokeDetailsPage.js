06d8d5cdcc194ff27281a1177da63c61e690cca8
carolini-constantino <lou_carol@hotmail.com>
2022-05-31 16:40:22 -0300

    enviando página de detalhes e estilização

--- pokedex/src/pages/PokeDetailsPage.js
+++ pokedex/src/pages/PokeDetailsPage.js
@@ -1,15 +1,90 @@
+import { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Header from "../components/Header";
-export default function PokeDetailsPage() {  
+import GlobalStateContext from "../global/GlobalStateContext";
+import styled from "styled-components";
 
-    return( 
+const StyleCardDetails = styled.div`
+text-align: center;
+border: 5px solid  #FECB05;
+background-color: #c9cdde;
+padding-bottom: 15px;
+font-family: Arial, Helvetica, sans-serif;
+color: #1F356B;
+img{
+    width: 20%;
+}
+`
 
-     <div>
-         <Header actualPage={"pokedetails"} />
+export default function PokeDetailsPage() {
+    const params = useParams();
+    const { states, getters } = useContext(GlobalStateContext);
+    const { pokemon } = states;
+    const { getPokeDetails } = getters;
 
+    useEffect(() => {
+        getPokeDetails(params.pokeName)
+    }, []);
 
-         <p>PokeDetails</p>
-     </div>
+    const pokeDetails = pokemon.name ? (
+        <>
 
-    ); 
+            <figure>
+                <h2>{pokemon.name.toUpperCase()}</h2>
+                <img src={pokemon.images.front} alt={`${pokemon.name} de frente`} />
+                <img src={pokemon.images.back} alt={`${pokemon.name} de costas`} />
+            </figure>
+
+            <section>
+                <h2>Status:</h2>
+                {pokemon.status.map((stat) => {
+                    return (
+                        <div key={stat["status_name"]}>
+                            <span>{stat["status_name"].toUpperCase()}: </span>
+                            <span>{stat.value}</span>
+                            <br />
+                        </div>
+
+                    )
+                })}
+            </section>
+
+            <section>
+                <h2>Tipos: </h2>
+                {pokemon.types.map((type) => {
+                    return (
+                        <li key={type}>{type}</li>
+                    )
+                })}
+            </section>
+
+            <section>
+                <h2>Habilidades: </h2>
+                {pokemon.abilities.filter((ability, index) => {
+                    if (index < 5) {
+                        return ability;
+                    }
+                }).map((ability) => {
+                    return (
+                        <li key={ability}>{ability}</li>
+                    )
+                })}
+            </section>
+
+        </>
+
+    ) : <p>CARREGANDO...</p>
+
+    return (
+        <div>
+            <Header actualPage={"pokedetails"} />
+            <hr />
+            <StyleCardDetails>
+                <h1>PokeInfos</h1>
+                {pokeDetails}
+            </StyleCardDetails>
+        </div>
+
+    );
 
 }; 
--- pokedex/src/pages/PokeListPage.js
+++ pokedex/src/pages/PokeListPage.js
@@ -1,50 +1,84 @@
-import { navigate, useState, useEffect } from "react";
+import { useEffect, useContext } from "react";
 import Header from "../components/Header";
-import axios from "axios";
-import { goToDetailsPage } from "../routes/coordinator";
-import { BASE_URL } from "../constants/urls";
+import PokeCard from "../components/PokeCard";
+import GlobalStateContext from "../global/GlobalStateContext";
+import styled from "styled-components";
 
+const StyledList = styled.div`
+text-align: center;
+border: 5px solid  #FECB05;
+background-color: #c9cdde;
+padding-bottom: 15px;
+font-family: Arial, Helvetica, sans-serif;
+color: #1F356B;
+
+span{
+    font-style: bold; 
+}
+button{
+  margin: 9px;
+  background-color: #13aa52;
+  border: 1px solid #13aa52;
+  border-radius: 4px;
+  box-shadow: rgba(0, 0, 0, .1) 0 2px 4px 0;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  font-family: "Akzidenz Grotesk BQ Medium", -apple-system, BlinkMacSystemFont, sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  outline: none;
+  outline: 0;
+  padding: 10px 25px;
+  text-align: center;
+  transform: translateY(0);
+  transition: transform 150ms, box-shadow 150ms;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+button:hover {
+  box-shadow: rgba(0, 0, 0, .15) 0 3px 9px 0;
+  transform: translateY(-2px);
+}
+
+@media (min-width: 768px) {
+  button {
+    padding: 10px 30px;
+  }
+}
+
+`
 export default function PokeListPage() {
-    const [list, setList] = useState ([]);
-
-
- 
-
-    const getList = () => {
-        axios.get(`${BASE_URL}/list?limit=20&offset=0`)
-            .then((res) => {
-                setList(res.data);
-            })
-            .catch((err) => {
-                alert(err.message);
-            });
-    };
+    const { states, getters } = useContext(GlobalStateContext)  
+    const { pokeList } = states
+    const { getPokeList } = getters;  
 
     useEffect(() => {
-        getList();
+        getPokeList();
     }, [])
 
-
-    const renderList = list.map((pokemon) => {
-        return(
-            <section key={pokemon.id}>
-            <span>{pokemon.name.toUpperCase()} - </span>
-            <span>Nº: {pokemon.id}</span>
-            <br />
-            <button>Adicionar a Pokedex</button>
-            <button onClick={() => goToDetailsPage(navigate)}>Ver detalhes</button>
-            <hr />
-        </section>
-            
-        )
-    })
+    const showPokeList = pokeList[0] ? pokeList.map((pokemon) => {
+        return  (
+            <PokeCard
+            key={pokemon.id}
+            pokemon={pokemon}
+            />
+        );
+    }) : <h2>Carregando...</h2>
 
     return (
 
         <div>
             <Header actualPage={"pokelist"} />
-            <p>Pokelist</p>
-                {renderList}
+            <main>
+            
+            <StyledList>
+            <h1>Lista de Pokemons</h1>
+                {showPokeList}
+            </StyledList>
+            </main>
         </div>
 
     );
